541cafe9c45c3bacc46b2ad4092ff01ee6d75a3c
deepakkumar <memishra9@gmail.com>
2025-01-11 18:47:41 +0530

    Commit changes

--- DataStructureandAlgorithm/DoublyLinkedList.java
+++ DataStructureandAlgorithm/DoublyLinkedList.java
@@ -1,1 +1,30 @@
 package DataStructureandAlgorithm;
+public class DoublyLinkedList{
+    Node head;
+    Node tail;
+    int length;
+
+    public class Node{
+        int data;
+        Node next;
+        Node previous;
+
+        Node(int data){
+            this.data=data;
+        }
+
+    }
+    DoublyLinkedList(int data){
+        Node newNode= new Node(data);
+        head=newNode;
+        tail=newNode;
+        length=1;
+    }
+
+
+    public static void main(String[] args) {
+        SinglyLinkedList sll= new SinglyLinkedList(1);
+        System.out.println(sll.head.data);
+    }
+
+}
