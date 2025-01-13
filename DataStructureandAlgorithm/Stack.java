168d97fba57818eea133eeb7ba9d1ab95d0d3d1b
deepakkumar <memishra9@gmail.com>
2025-01-13 19:03:18 +0530

    Commit changes

--- DataStructureandAlgorithm/Stack.java
+++ DataStructureandAlgorithm/Stack.java
@@ -1,1 +1,63 @@
 package DataStructureandAlgorithm;
+
+import java.util.EmptyStackException;
+
+public class Stack{
+    private Node top;
+    private int length;
+    private class Node {
+       private int data;
+       private Node next;
+
+        Node(int data){
+            this.data=data;
+        }
+    }
+    public Stack(){
+        top=null;
+        length=0;
+
+    }
+    public int length(){
+        return length;
+    }
+    public boolean isEmpty(){
+        return length==0;
+    }
+
+    public void push(int data){
+        Node tmp= new Node(data);
+        tmp.next=top;
+        top=tmp;
+        length++;
+
+
+    }
+
+    public int pop(){
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+        int result= top.data;
+        top =top.next;
+        length--;
+        return result;
+    }
+    public int peek(){
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+        return top.data;
+    }
+
+    public static void main(String[] args) {
+        Stack s= new Stack();
+        System.out.println("Length is :" +s.length());
+        s.push(10);
+        s.push(15);
+        s.push(20);
+        s.pop();
+        System.out.println(s.peek());
+
+    }
+}
